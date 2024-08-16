38131a76a013ccee59fdc9b1bbaa7b453becddb4
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2024-08-15 20:09:26 -0400

    part 3d
    adding validation and eslint (exercises 3.21-3.22)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -10,94 +10,95 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    'id': '1',
+    'name': 'Arto Hellas',
+    'number': '040-123456'
+  },
+  {
+    'id': '2',
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
+  },
+  {
+    'id': '3',
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
+  },
+  {
+    'id': '4',
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
+  }
 ]
 
 const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(p => Number(p.id)))
-      : 0
-    return String(maxId + 1)
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => Number(p.id)))
+    : 0
+  return String(maxId + 1)
 }
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    console.log(body);
-  
-    if (body === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    // if(persons.some(p => p.name == body.name)){
-    //     return response.status(400).json({ 
-    //         error: 'name must be unique' 
-    //       })
-    // }
+  // if (body === undefined) {
+  //   return response.status(400).json({ error: 'content missing' })
+  // }
 
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    })
-  
-    person.save().then(savedPerson => {
+  // if(persons.some(p => p.name == body.name)){
+  //     return response.status(400).json({
+  //         error: 'name must be unique'
+  //       })
+  // }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook had info for ${persons.length} people<br/>${new Date().toString()}`)
+  response.send(`Phonebook had info for ${persons.length} people<br/>${new Date().toString()}`)
 })
 
 //const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 const unknownEndpoint = (request, response) => {
@@ -111,7 +112,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
